c6c248a2ddf8eb4f04e9bbda70456d3a7db0ed22
Saurab Dulal <sdulal@memphis.edu>
2018-11-16 10:20:47 -0600

    sequence number and user details table deployed

--- user_data/birb.js
+++ user_data/birb.js
@@ -4,12 +4,25 @@ const electron = require('electron');
 const app = electron.app || electron.remote.app;
 
 function createUser(username,flag) {
-    if (flag=='L') var tablename = "L_"+username;
-    else var tablename = "R_"+username
+    if (flag=='L') {
+      var user_birb = "L_"+username+"_birb";
+      var user_details = "L_"+username+"_details";
+    }
+    else {
+      var user_birb = "R_"+username+"_birb";
+      var user_details = "R_"+username+"_details";
+    }
+    // create two tables, one for storing birb, another for storing user details
 
-    db.createTable(tablename, (succ, msg) => {
+    db.createTable(user_birb, (succ, msg) => {
       // succ - boolean, tells if the call is successful
       console.log("Success: " + succ);
       console.log("Message: " + msg);
-    })
+    });
+
+    db.createTable(user_details, (succ, msg) => {
+      // succ - boolean, tells if the call is successful
+      console.log("Success: " + succ);
+      console.log("Message: " + msg);
+    });
 }
