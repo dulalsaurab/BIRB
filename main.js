994f03e09551130c16e112796ed90049beff4a70
Saurab Dulal <sdulal@memphis.edu>
2018-11-19 00:26:33 -0600

    function build for follow, and profile, and made several changes

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const path = require('path');
 const Store = require('./user_data/store.js');
 // const Store = require('./lib/user_management.js');
 const birb = require('./user_data/birb.js');
-const _nfd = require('./lib/nfd_operations.js')
+const _nfd = require('./lib/nfd_operations.js');
+const um = require('./lib/user_management');
 
 /*
 
@@ -15,74 +16,6 @@ const _nfd = require('./lib/nfd_operations.js')
   check if user table exists or not, if exist, load homepage else load loging page
 
 */
-
-const fs = require('fs');
-const os = require('os');
-var shell = require('shelljs');
-global.username = "someting";
-
-function getUsername(path){
-
-  var res = path.split("/");
-  console.log(res)
-  for (var item in res){
-    var boo = res[item].includes(".json")
-    /*
-      this section needs future attention, constant values are checked,
-      needs to replace it with some generic solution
-    */
-    if (boo)
-    {
-      var candidate = res[item].replace(".json", "")
-      try {
-        var withouthPrefix = candidate.replace("L_","");
-        var withoutSuffix = withouthPrefix.replace("_birb","");
-        return withoutSuffix
-      } catch (e) {
-        console.log(e);
-        continue;
-      }
-    }
-  }
-
-}
-
-function checkIfUserExist(){
-  // get platform name, and the location where user data is stored
-  const platform = os.platform();
-  let appName = '';
-  if (JSON.parse(fs.readFileSync('package.json', 'utf-8')).productName) {
-    appName = JSON.parse(fs.readFileSync('package.json', 'utf-8')).productName;
-  }else{
-    appName = JSON.parse(fs.readFileSync('package.json', 'utf-8')).name;
-  }
-  let userdata='';
-  if (platform === 'win32') {
-    userData = path.join(process.env.APPDATA, appName);
-  } else if (platform === 'darwin') {
-    userData = path.join(process.env.HOME, 'Library', 'Application Support', appName);
-  } else {
-    userData = path.join('var', 'local', appName);
-  }
-  /*
-    since we have modified the user data storage location to userData,
-    it is appended with the path userData
-  */
-  userData=userData+"/userData"
-  /*
-    user file always starts with L_*,
-    so if it exists users is already register
-  */
-  var outPut = shell.ls(userData+'/L_*.json');
-  if (outPut['stdout']=='') {console.log("No register user found on the system"); return false}
-  else {
-    console.log("user already register");
-    /* get the user name*/
-    global.username = getUsername(outPut['stdout'])
-    return true
-  }
-}
-
 
 let mainWindow
 
@@ -140,7 +73,7 @@ app.on('ready', function() {
     // Now that we have them, save them using the `set` method.
     store.set('windowBounds', { width, height });
   });
-  var flag = checkIfUserExist()
+  var flag = um.checkIfUserExist();
   if (flag){
     mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'));
   }
@@ -167,5 +100,10 @@ app.on('activate', function () {
   }
 })
 
+// Export the public available functions
+module.exports = {
+  // getUsername,
+  // getAppDataPath
+};
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
