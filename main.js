599f977589dd7b7767d57cce7c4c7d63300a8050
Saurab Dulal <sdulal@memphis.edu>
2018-11-20 22:29:51 -0600

    send interest and receive data completed

--- main.js
+++ main.js
@@ -1,114 +1,76 @@
 /*
-  Modules to control application life and create native browser window
+  Modules to control application and create browser windows
 */
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow} = require('electron');
 const path = require('path');
-const Store = require('./user_data/store.js');
+const StoreData = require('./user_data/store.js');
 // const Store = require('./lib/user_management.js');
 const birb = require('./user_data/birb.js');
 const _nfd = require('./lib/nfd_operations.js');
 const um = require('./lib/user_management');
 
-/*
 
-  Keep a global reference of the window object, if you don't, the window will
-  be closed automatically when the JavaScript object is garbage collected.
-  check if user table exists or not, if exist, load homepage else load loging page
+let mainWindow;
 
-*/
-
-let mainWindow
-
-
-// First instantiate the class
-const store = new Store({
-  // We'll call our data file 'user-preferences'
-  configName: 'user-preferences',
-  defaults: {
-    // 800x600 is the default size of our window
-    windowBounds: { width: 800, height: 600 }
-  }
+const store = new StoreData({
+    configName: 'user-preferences',
+    defaults: {
+        // 800x600 is the default size of our window
+        windowBounds: { width: 800, height: 600 }
+    }
 });
 
 function createWindow () {
-  // Create the browser window.
+    // Create the browser window.
 
-  let { width, height } = store.get('windowBounds');
+    let { width, height } = store.get('windowBounds');
 
-  mainWindow = new BrowserWindow({width, height})
+    mainWindow = new BrowserWindow({width, height});
+    mainWindow.loadFile('login.html');
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('login.html')
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-
-// When our app is ready, we'll create our BrowserWindow
 app.on('ready', function() {
 
-  // First we'll get our height and width. This will be the defaults if there wasn't anything saved
-  let { width, height } = store.get('windowBounds');
+    let { width, height } = store.get('windowBounds');
 
-  // Pass those values in to the BrowserWindow options
-  mainWindow = new BrowserWindow({ width, height });
+    mainWindow = new BrowserWindow({ width, height });
 
-  // The BrowserWindow class extends the node.js core EventEmitter class, so we use that API
-  // to listen to events on the BrowserWindow. The resize event is emitted when the window size changes.
-  mainWindow.on('resize', () => {
-    // The event doesn't pass us the window size, so we call the `getBounds` method which returns an object with
-    // the height, width, and x and y coordinates.
-    let { width, height } = mainWindow.getBounds();
-    // Now that we have them, save them using the `set` method.
-    store.set('windowBounds', { width, height });
-  });
-  var flag = um.checkIfUserExist();
-  if (flag){
-    mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'));
-  }
-  else {
-    mainWindow.loadURL('file://' + path.join(__dirname, 'login.html'));
-  }
+    mainWindow.on('resize', () => {
+        let { width, height } = mainWindow.getBounds();
+        store.set('windowBounds', { width, height });
+    });
+    var flag = um.checkIfUserExist();
+    if (flag){
+        mainWindow.loadURL('file://' + path.join(__dirname, 'index.html'));
+    }
+    else {
+        mainWindow.loadURL('file://' + path.join(__dirname, 'login.html'));
+    }
 });
 
-
-// Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
 
-
-
-
-  }
-})
+    if (mainWindow === null) {
+        createWindow()
+    }
+});
 
 // Export the public available functions
 module.exports = {
-  // getUsername,
-  // getAppDataPath
+    // getUsername,
+    // getAppDataPath
 };
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+//All the main processes can go here, otherwise, it can go on some other file but needs require here
+
--- user_data/birb.js
+++ user_data/birb.js
@@ -1,3 +1,14 @@
+
+/*
+
+  This script will help to do the create tables for local and remote user
+  Status:
+  Author: @Saurab Dulal
+
+*/
+
+
+
 const db = require('electron-db');
 const electron = require('electron');
 
@@ -5,12 +16,12 @@ const app = electron.app || electron.remote.app;
 
 function createUser(username,flag) {
     if (flag=='L') {
-      var user_birb = "L_"+username+"_birb";
-      var user_details = "L_"+username+"_details";
+        var user_birb = "L_"+username+"_birb";
+        var user_details = "L_"+username+"_details";
     }
     else {
-      var user_birb = "R_"+username+"_birb";
-      var user_details = "R_"+username+"_details";
+        var user_birb = "R_"+username+"_birb";
+        var user_details = "R_"+username+"_details";
     }
     /*
       create two tables, one for storing birb,
@@ -18,14 +29,14 @@ function createUser(username,flag) {
     */
 
     db.createTable(user_birb, (succ, msg) => {
-      // succ - boolean, tells if the call is successful
-      console.log("Success: " + succ);
-      console.log("Message: " + msg);
+        // succ - boolean, tells if the call is successful
+        console.log("Success: " + succ);
+        console.log("Message: " + msg);
     });
 
     db.createTable(user_details, (succ, msg) => {
-      // succ - boolean, tells if the call is successful
-      console.log("Success: " + succ);
-      console.log("Message: " + msg);
+        // succ - boolean, tells if the call is successful
+        console.log("Success: " + succ);
+        console.log("Message: " + msg);
     });
 }
